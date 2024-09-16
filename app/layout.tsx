c67603d953fe85bb3a034b18542427a6faebc975
gorhom <gorhom.dev@gmail.com>
2024-09-16 23:22:07 +0200

    added dummy placeholder site

---
+++ app/layout.tsx
@@ -0,0 +1,62 @@
+import './global.css'
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import { Navbar } from './components/nav'
+import Footer from './components/footer'
+import { baseUrl } from './sitemap'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Mo Gorhom',
+    template: '%s | Mo Gorhom',
+  },
+  description: 'A simple personal site.',
+  openGraph: {
+    title: 'Mo Gorhom',
+    description: 'A simple personal site.',
+    url: baseUrl,
+    siteName: 'Mo Gorhom',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+}
+
+const cx = (...classes) => classes.filter(Boolean).join(' ')
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          {/* <Navbar /> */}
+          {children}
+          {/* <Footer /> */}
+        </main>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Mo Gorhom
+      </h1>
+      <p className="mb-4">
+        🚧 ...
+      </p>
+      <div className="my-8">
+        {/* <BlogPosts /> */}
+      </div>
+    </section>
+  )
+}
